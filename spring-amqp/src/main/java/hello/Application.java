fdb4c1a0a774613ba41c8423656faf767a86f5b4
Bibhudatta Jena <bjena@manh.com>
2019-09-17 22:15:03 -0400

    Docker-rabbitmq-spring-program

---
+++ spring-amqp/src/main/java/hello/Application.java
@@ -0,0 +1,60 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    private static final Logger logger =
+            LoggerFactory.getLogger(Application.class);
+    static final String topicExchangeName = "spring-amqp-exchange";
+    static final String queueName = "q1";
+    static final String routingKey = "q1";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(
+            ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container =
+                new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        SpringApplication.run(Application.class, args).close();
+    }
+}
---
+++ spring-amqp/src/main/java/hello/Receiver.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+    private static final Logger logger =
+            LoggerFactory.getLogger(Receiver.class);
+
+    public void receiveMessage(String message) {
+        logger.debug("Received <" + message + ">");
+    }
+}
