aa9bafbe6e5073419a9e66d497fa93da6f1f181f
Bibhudatta Jena <bjena@manh.com>
2019-09-20 16:05:33 -0400

    Auth server - Okta implementation

---
+++ Okta_demo/AuthClientOkta/src/main/java/com/okta/spring/OktaOAuthClient/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.okta.spring.OktaOAuthClient;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
---
+++ Okta_demo/SimpleAuthClient/src/main/java/com/okta/spring/SpringBootAuthClient/SpringBootAuthClientApplication.java
@@ -0,0 +1,13 @@
+package com.okta.spring.SpringBootAuthClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthClientApplication.class, args);
+	}
+
+}
